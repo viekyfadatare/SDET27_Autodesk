d733fdeb35e9d4210bebbf19b69f8a12d4b3f2e1
admin <admin@DESKTOP-BP7NV3R>
2021-12-29 17:38:51 +0530

    This Is GitHub Practice

---
+++ src/test/java/Practice/GItHubPractice_1.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class GItHubPractice_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("==== Hi I Am GitHub ====");
+		
+	}
+
+}
